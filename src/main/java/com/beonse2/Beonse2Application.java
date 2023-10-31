f21986a66f07ba3c9351987ab75e2cffae9c0e83
HyeGwang Kang <kingaser@naver.com>
2023-10-31 16:36:16 +0900

    feat : 개발 환경 설정

---
+++ src/main/java/com/beonse2/Beonse2Application.java
@@ -0,0 +1,13 @@
+package com.beonse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beonse2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Beonse2Application.class, args);
+    }
+
+}
