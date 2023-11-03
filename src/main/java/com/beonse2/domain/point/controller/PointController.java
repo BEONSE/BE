ae8d8355d7790c3b0923f3d9d1f5f5eb21a3a5a9
HyeGwang Kang <kingaser@naver.com>
2023-11-03 19:53:33 +0900

    feat : Point Controller 생성

---
+++ src/main/java/com/beonse2/domain/point/controller/PointController.java
@@ -0,0 +1,10 @@
+package com.beonse2.domain.point.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PointController {
+
+}
