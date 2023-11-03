5266350a7702ce1f97793b8e6be86bda1e0fce58
HyeGwang Kang <kingaser@naver.com>
2023-11-03 16:53:41 +0900

    feat : Global Exception 처리 및 예외 상태 코드 ENUM 추가

---
+++ src/main/java/com/beonse2/config/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.beonse2.config.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+}
