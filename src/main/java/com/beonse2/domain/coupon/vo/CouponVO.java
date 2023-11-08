19540b10576fd1b0c724ad7c541d2a42faaf7847
HyeGwang Kang <kingaser@naver.com>
2023-11-08 14:07:30 +0900

    feat : 쿠폰 VO 객체 생성

---
+++ src/main/java/com/beonse2/domain/coupon/vo/CouponVO.java
@@ -0,0 +1,32 @@
+package com.beonse2.domain.coupon.vo;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+import static lombok.AccessLevel.*;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class CouponVO {
+
+    private Long cid;
+    private Long memberMid;
+    private Long branchBid;
+    private String type;
+    private int price;
+    private boolean isUsed;
+    private Timestamp paymentDate;
+
+    @Builder
+    public CouponVO(Long memberMid, Long branchBid, String type, int price, boolean isUsed) {
+        this.memberMid = memberMid;
+        this.branchBid = branchBid;
+        this.type = type;
+        this.price = price;
+        this.isUsed = isUsed;
+    }
+}
