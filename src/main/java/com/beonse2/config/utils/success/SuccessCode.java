0c0c69a0942e94a615c3fc47bba7e737483f3eb5
HyeGwang Kang <kingaser@naver.com>
2023-11-09 14:31:21 +0900

    feat : 게시글 작성 성공 코드 추가

--- src/main/java/com/beonse2/config/utils/success/SuccessCode.java
+++ src/main/java/com/beonse2/config/utils/success/SuccessCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     SUCCESS_DELETE_BOARD(HttpStatus.OK.value(), "게시글이 정상적으로 삭제되었습니다"),
-    SUCCESS_SAVE_COUPON(HttpStatus.OK.value(), "쿠폰이 정상적으로 등록되었습니다.");
+    SUCCESS_SAVE_COUPON(HttpStatus.CREATED.value(), "쿠폰이 정상적으로 등록되었습니다."),
+    SUCCESS_CREATE_BOARD(HttpStatus.CREATED.value(), "게시글이 등록되었습니다.");
 
     private final int statusCode;
     private final String successMessage;
