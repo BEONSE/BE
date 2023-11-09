4fb64ffe51dd9f9c57eb16fd693898def8dc94cf
HyeGwang Kang <kingaser@naver.com>
2023-11-09 15:22:23 +0900

    feat : 메이트 게시글 전체 조회 SQL

--- src/main/java/com/beonse2/domain/mate/board/mapper/MateBoardMapper.java
+++ src/main/java/com/beonse2/domain/mate/board/mapper/MateBoardMapper.java
@@ -1,10 +1,15 @@
 package com.beonse2.domain.mate.board.mapper;
 
+import com.beonse2.domain.mate.board.dto.MateBoardResponseDTO;
 import com.beonse2.domain.mate.board.vo.MateBoardVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MateBoardMapper {
 
     void saveMateBoard(MateBoardVO mateBoardVO);
+
+    List<MateBoardResponseDTO> findAll();
 }
