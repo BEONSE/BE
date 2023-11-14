78263556185d54225f3ed1c7d0bd0a04536c8a76
HyeGwang Kang <kingaser@naver.com>
2023-11-14 14:43:50 +0900

    feat : MateBoardVO 객체 생성

---
+++ src/main/java/com/beonse2/domain/mate/board/vo/MateBoardVO.java
@@ -0,0 +1,28 @@
+package com.beonse2.domain.mate.board.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Getter
+@NoArgsConstructor
+public class MateBoardVO {
+
+    private Long mbid;
+    private Long memberMid;
+    private String title;
+    private String content;
+    private boolean status;
+    private Timestamp createdAt;
+    private Timestamp modifiedAt;
+
+    @Builder
+    public MateBoardVO(Long memberMid, String title, String content, boolean status) {
+        this.memberMid = memberMid;
+        this.title = title;
+        this.content = content;
+        this.status = status;
+    }
+}
