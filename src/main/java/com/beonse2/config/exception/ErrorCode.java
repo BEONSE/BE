5bef694ceeef48bfd00ae1e5126caf420e3afcc9
HARA1029 <gusdud4929@gmail.com>
2023-11-17 18:08:37 +0900

    feat : 가맹점 멤버별 쿠폰 사용 조회

--- src/main/java/com/beonse2/config/exception/ErrorCode.java
+++ src/main/java/com/beonse2/config/exception/ErrorCode.java
@@ -30,7 +30,6 @@ public enum ErrorCode {
     NOT_MATCH_USER(HttpStatus.FORBIDDEN.value(), "작성자가 아닙니다."),
     NOT_MATCH_BRANCH(HttpStatus.FORBIDDEN.value(), "일치하는 가맹점이 없습니다."),
     NOT_FOUND_INFO(HttpStatus.NOT_FOUND.value(), "회원 정보를 찾을 수 없습니다."), 
-    NOT_FOUND_INFO(HttpStatus.NOT_FOUND.value(), "회원 정보를 찾을 수 없습니다."),
     DONT_WRITE_REVIEW(HttpStatus.FORBIDDEN.value(), "쿠폰 사용자만 리뷰를 작성할 수 있습니다."),
     ALREADY_WRITTEN_REVIEW(HttpStatus.FORBIDDEN.value(), "이미 리뷰를 작성한 쿠폰입니다.");
 
