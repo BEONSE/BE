979f49254f179894a6eb69f2163493f453041df5
elane <chagaram0610@gmail.com>
2023-11-25 18:06:08 +0900

    feat : 메이트 게시판 지점명 추가

--- src/main/java/com/beonse2/domain/mate/board/dto/MateBoardResponseDTO.java
+++ src/main/java/com/beonse2/domain/mate/board/dto/MateBoardResponseDTO.java
@@ -3,16 +3,19 @@ package com.beonse2.domain.mate.board.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
 @Getter
+@NoArgsConstructor
 public class MateBoardResponseDTO {
 
     private Long mbid;
     private String title;
     private String content;
     private String nickname;
+    private String branchName;
     private int paymentAmount;
     private byte[] imageData;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
@@ -23,11 +26,12 @@ public class MateBoardResponseDTO {
     private int grade;
 
     @Builder
-    public MateBoardResponseDTO(Long mbid, String title, String content, String nickname, int paymentAmount, byte[] imageData, Timestamp createdAt, Timestamp modifiedAt) {
+    public MateBoardResponseDTO(Long mbid, String title, String content, String nickname, String branchName, int paymentAmount, byte[] imageData, Timestamp createdAt, Timestamp modifiedAt) {
         this.mbid = mbid;
         this.title = title;
         this.content = content;
         this.nickname = nickname;
+        this.branchName = branchName;
         this.paymentAmount = paymentAmount;
         this.imageData = imageData;
         this.createdAt = createdAt;
